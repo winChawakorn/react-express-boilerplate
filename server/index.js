4f35a59bf9b7a208f6998d1d84d5320da0f44388
winChawakorn <chawakorn.su@ku.th>
2018-12-30 00:35:57 +0700

    Add body-parser

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import applications from './routes/applications'
 import express from 'express'
 import moment from 'moment'
+import bodyParser from 'body-parser'
 
 const app = express()
-const PORT = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`[${moment().format()}] Server started on port ${PORT}`)
