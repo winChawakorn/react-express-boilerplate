ea38b1598b338d7e6ad3c481cf35863fb162b387
winChawakorn <chawakorn.su@ku.th>
2019-04-14 02:52:15 +0700

    Change from mongodb to mysql

--- server/config.js
+++
@@ -1,7 +0,0 @@
-exports.mysql_connection = {
-  host: process.env.MYSQL_HOST || '127.0.0.1',
-  port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB || 'munkong',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-}
--- server/misc/config.js
+++ server/misc/config.js
@@ -28,6 +28,14 @@ const config = {
   defaultTimeZone: env.DEFAULT_TIMEZONE || '+0700',
   loglevel: env.LOGLEVEL || 'verbose',
   maxfilesize: env.MAX_FILE_SIZE || 1000000, // 1MB
+  mysql_connection: {
+    host: process.env.MYSQL_HOST || '127.0.0.1',
+    port: process.env.MYSQL_PORT || '3306',
+    database: process.env.MYSQL_DB || 'munkong',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || 'password',
+  }
+
 }
 
 /**
